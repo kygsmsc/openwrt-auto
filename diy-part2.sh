f8cbe23e3900f7e93663d9fc0ebba66231e15cab
kygsmsc <59911411+kygsmsc@users.noreply.github.com>
2026-07-02 11:55:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -64,12 +64,24 @@ sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" packa
 #echo "修改默认主题"
 #sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
-# ---------- 新增：luci-app-run ----------
-echo ">>> 添加 luci-app-run"
+# ===============================================
+# 添加 luci-theme-istore（iStoreOS 蓝白主题）
+# ===============================================
+echo ">>> [part2] 添加 luci-theme-istore"
+rm -rf package/luci-theme-istore
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-istore.git package/luci-theme-istore
+
+# ===============================================
+# 添加 luci-app-run（悟空Daily）
+# ===============================================
+echo ">>> [part2] 添加 luci-app-run"
 rm -rf package/luci-app-run
 git clone --depth=1 https://github.com/wukongdaily/luci-app-run.git package/luci-app-run
-echo "DIY2 完成"
+
+echo ">>> [part2] 完成"
+
 echo 'CONFIG_PACKAGE_luci-app-run=y' >> .config
+echo 'CONFIG_PACKAGE_luci-theme-istore=y' >> .config
 
 #if [ $delete_bootstrap ]; then
 #  echo "去除默认bootstrap主题"
