ea201c85729ce07fcc1525a30fc4648e0204bb29
kygsmsc <59911411+kygsmsc@users.noreply.github.com>
2026-07-04 14:51:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -67,9 +67,9 @@ sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" packa
 # ===============================================
 # 添加 luci-theme-istore（iStoreOS 蓝白主题）
 # ===============================================
-echo ">>> [part2] 添加 luci-theme-argon"
-rm -rf package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# echo ">>> [part2] 添加 luci-theme-argon"
+# rm -rf package/luci-theme-argon
+# git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
 # ===============================================
 # 添加 luci-app-run（悟空Daily）
@@ -80,17 +80,17 @@ git clone --depth=1 https://github.com/wukongdaily/luci-app-run.git package/luci
 
 echo ">>> [part2] 完成"
 # -------- iStore --------
-echo 'CONFIG_PACKAGE_luci-app-store=y' >>.config
-echo 'CONFIG_PACKAGE_luci-theme-istore=y' >>.config
+# echo 'CONFIG_PACKAGE_luci-app-store=y' >>.config
+# echo 'CONFIG_PACKAGE_luci-theme-istore=y' >>.config
 
 # -------- luci-app-run --------
 echo 'CONFIG_PACKAGE_luci-app-run=y' >>.config
 
 # -------- Docker --------
-echo 'CONFIG_PACKAGE_docker=y' >>.config
-echo 'CONFIG_PACKAGE_dockerd=y' >>.config
-echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >>.config
-echo 'CONFIG_PACKAGE_luci-lib-docker=y' >>.config
+# echo 'CONFIG_PACKAGE_docker=y' >>.config
+# echo 'CONFIG_PACKAGE_dockerd=y' >>.config
+# echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >>.config
+# echo 'CONFIG_PACKAGE_luci-lib-docker=y' >>.config
 
 #if [ $delete_bootstrap ]; then
 #  echo "去除默认bootstrap主题"
